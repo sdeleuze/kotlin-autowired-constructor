16ded9d0deecd912ad9347a875818ae2b99f67a7
Jon Schneider <jkschneider@gmail.com>
2016-04-04 10:00:58 -0700

    Fixed sample to demonstrate @Autowired problem

--- src/main/kotlin/com/example/DemoApplication.kt
+++ src/main/kotlin/com/example/DemoApplication.kt
@@ -1,18 +1,22 @@
 package com.example
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Repository
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-open class DemoApplication {
-
-    @Bean
-    open fun foo() = Foo()
-
-}
+open class DemoApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(DemoApplication::class.java, *args)
 }
 
+interface DemoRepository
+@Repository open class DemoRepositoryImpl(): DemoRepository
+open class NoOpDemoRepository(): DemoRepository
+
+@RestController
+open class DemoController @Autowired/*(required = false)*/ constructor(
+        val demoRepo: DemoRepository = NoOpDemoRepository())
--- src/test/kotlin/com/example/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/DemoApplicationTests.kt
@@ -1,18 +1,22 @@
 package com.example
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.web.WebAppConfiguration
 
 @RunWith(SpringJUnit4ClassRunner::class)
 @SpringApplicationConfiguration(classes = arrayOf(DemoApplication::class))
 @WebAppConfiguration
 class DemoApplicationTests {
+    @Autowired lateinit var controller: DemoController
 
 	@Test
 	fun contextLoads() {
+        assertTrue(controller.demoRepo is DemoRepositoryImpl)
 	}
 
 }
